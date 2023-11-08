e7eeb1e415a7a7d1506a9ff2c5858c566b8672b2
Ivan Martirena <ivanmartirena45@gmail.com>
2023-11-08 20:46:04 -0300

    09-11-2023 solucion js

--- assets/producto.js
+++ assets/producto.js
@@ -1,10 +1,4 @@
-window.onload = function () {
-  //Catalogo => Producto
-  let $card_producto = document.querySelector(".card_producto");
-  if($card_producto){
-    
-    card_producto.addEventListener("click", function(){
-      window.location.href = "producto.html";
-  });
-}
+
+function ver_producto(){
+    window.location.href = "producto.html";
 }
